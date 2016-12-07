6112a0c694754016b317a6365987f4d3576fb98b
Maria <mariam9010@gmail.com>
2016-12-07 14:05:52 +0200

    firstarray

--- test4/test4/Program.cs
+++ test4/test4/Program.cs
@@ -11,7 +11,30 @@ namespace test4
         static void Main(string[] args)
         {
         }
-        hytr
+        interface IWow
+        {
+            int[] GetTreatedArray(int[] arr);
+        }
+
+        public class FirstArray : IWow
+        {
+            int[] GetTreatedArray(int[] arr)
+            {
+                int a = 10;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] >= a)
+                    {
+                        a = arr[i]; свввв
+                    }
+                }
+
+                return a;
+
+
+
+            }
+        }
 
     }
 }
